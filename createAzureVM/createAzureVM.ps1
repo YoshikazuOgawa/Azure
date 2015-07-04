cea2710a75903300e4f385597b50e6a014015799
root <root@www.akausagi.info>
2015-07-05 02:46:36 +0900

    modify createAzureVM.ps1

--- createAzureVM/createAzureVM.ps1
+++ createAzureVM/createAzureVM.ps1
@@ -1,16 +1,17 @@
-$success=0
-$failure=1
+# CONSTANT
+Set-Variable -name SUCCESS -value 0 -option constant
+Set-Variable -name FAILURE -value 1 -option constant
 
 # Add Azure account
 Add-AzureAccount
 
 # Get Azure VM Image
 $flg = 1
-while ( $flg -ne $success )
+while ( $flg -ne $SUCCESS )
 {
   $input_vmimage_name = Read-Host "Please input Azure VM image name"
   New-AzureVMImage -ImageName "$input_vmimage_name"
-  if ( $? -eq $success )
+  if ( $? -eq $SUCCESS )
   {
     $image = New-AzureVMImage -ImageName "$input_vmimage_name"
     Write-Output "Get AzureVMImage success."
@@ -25,13 +26,13 @@ while ( $flg -ne $success )
 
 # Create Azure configuration
 $flg = 1
-while ( $flg -ne $success )
+while ( $flg -ne $SUCCESS )
 {
   $input_hostname = Read-Host "Please input azure vm host name"
   $input_instance_size = Read-Host "Please input azure vm instance size"
   $input_availability_name = Read-Host "Please input azure availability name"
   New-AzureVMConfig -Name $input_hostname -InstanceSize $input_instance_size -Image $image.ImageName -AvalabilitySetName $input_avalability_name
-  if ( $? -eq $success )
+  if ( $? -eq $SUCCESS )
   {
     $vm = New-AzureVMConfig -Name $input_hostname -InstanceSize $input_instance_size -Image $image.ImageName -AvalabilitySetName $input_avalability_name
     Write-Output "Create new azure vm configuration success."
@@ -47,7 +48,7 @@ while ( $flg -ne $success )
 # Add Azure provisioning configuration and create Administrator's login information
 $flg = 1
 $input_os_type = "default"
-while ( $flg -ne $success )
+while ( $flg -ne $SUCCESS )
 {
   $input_os_adminuser_name = Read-Host "Please input OS administration user's name"
   $input_os_adminuser_passwd = Read-Host "Please input OS administration user's password"
@@ -59,24 +60,24 @@ while ( $flg -ne $success )
     default { Add-AzureProvisioningConfig -VM $vm $input_os_type -LinuxUser $input_os_adminuser_name -Password $input_os_adminuser_passwd }
   }
 
-  if ( $? -eq $success )
+  if ( $? -eq $SUCCESS )
   {
-     Write-Output "Add Azure provisioning configuration success."
+     Write-Output "Add Azure provisioning configuration SUCCESS."
      flg = 0
   }
   else
   {
-     Write-Output "Add Azure provisioning configuration failure."
+     Write-Output "Add Azure provisioning configuration FAILURE."
      flg = 1
   }
 }
 
 # Add Azure VM network interface card
 $flg = 1
-$nic_type = 0
-while ( $flg -ne $success )
+$input_nic_type = "default"
+while ( $flg -ne $SUCCESS )
 {
-  $input_nic_type = Read-Host "Please input nic type single or multi[default: single]"
+  $input_nic_type = Read-Host "Please input nic type 'single' or 'multi' [default: single]"
   switch -case ( $input_nic_type )
   {
     single { $nic_type = 0 }
@@ -92,12 +93,12 @@ while ( $flg -ne $success )
     Set-AzureSubnet -SubnetNames $input_subnet_name[0] -VM $vm
     if ( $? -eq 0 )
     {
-      Write-output "finish first network interface card configuration."
+      Write-output "finish 1st network interface card configuration."
       $flg = 0
     }
     else
     { 
-      Write-output "fail first network interface card configuration."
+      Write-output "fail 1st network interface card configuration."
       $flg = 1
     }
   }
@@ -107,10 +108,45 @@ while ( $flg -ne $success )
     $input_subnet_name[0] = Read-Host "Please input Azure subnet name(for first nic network)"
     $input_vnet_ip[0] = Read-Host "Please input Azure virtual network IP address(for firt nic ip address)"
     Set-AzureSubnet -SubnetNames $input_subnet_name[0] -VM $vm
+    if ( $? -eq 0 )
+    {
+      Write-output "finish 1st network interface card configuration."
+      $flg = 0
+    }
+    else
+    { 
+      Write-output "fail 1st network interface card configuration."
+      $flg = 1
+    }
 
     Write-Output "start multi-nic configuration."
-    $input_subnet_name[1] = Read-Host "Please input Azure subnet name(for second nic network)"
-    $input_vnet_ip[1] = Read-Host "Please input Azure virtual network IP address(for second nic ip address)"
+    $input_subnet_name[1] = Read-Host "Please input Azure subnet name(for 2nd nic network)"
+    $input_vnet_ip[1] = Read-Host "Please input Azure virtual network IP address(for 2nd nic ip address)"
+    Add-AzureNetworkInterfaceConfig -Name NIC1 -SubnetName $input_subnet_name[1] -StaticVNetIPAddress $input_vnet_ip[1] -VM $vm
+    if ( $? -eq 0 )
+    {
+      Write-output "finish 2nd network interface card configuration."
+      $flg = 0
+    }
+    else
+    { 
+      Write-output "fail 2nd network interface card configuration."
+      $flg = 1
+    }
+    
+    $input_subnet_name[2] = Read-Host "Please input Azure subnet name(for 3rd nic network)"
+    $input_vnet_ip[2] = Read-Host "Please input Azure virtual network IP address(for 3rd nic ip address)"
+    Add-AzureNetworkInterfaceConfig -Name NIC1 -SubnetName $input_subnet_name[2] -StaticVNetIPAddress $input_vnet_ip[2] -VM $vm
+    if ( $? -eq 0 )
+    {
+      Write-output "finish 3rd network interface card configuration."
+      $flg = 0
+    }
+    else
+    { 
+      Write-output "fail 3rd network interface card configuration."
+      $flg = 1
+    }
   }
   else 
   {
@@ -118,3 +154,6 @@ while ( $flg -ne $success )
     $flg = 1
   }
 }
+
+# Create VM
+
