4a8929055f3ca1e0060a207c1db55e26ad60d868
unknown <Administrator@DC.hoge.local>
2015-07-26 18:30:23 +0900

    modify create-virtualnetwork-xml.ps1

--- configureAzure/create-virtualnetwork-xml.ps1
+++ configureAzure/create-virtualnetwork-xml.ps1
@@ -12,19 +12,12 @@ $input_subnet_num = Read-Host "Please enter create subnet number"
 $input_subnet_name += $input_subnet_num
 $input_subnet_prefix += $input_subnet_num
 
-#for ( $i = 0; $i -lt $input_subnet_num; $i++ )
-#{
-#  $subnet_num = $i + 1
-#  $input_subnet_name[$i] = Read-Host "Please enter Subnet name (number $subnet_num)"
-#  $input_subnet_prefix[$i] = Read-Host "Please enter Subnet prefix (number $subnet_num)"
-#}
-
 echo '<?xml version="1.0" encoding="utf-8"?>' >> $VNETCONF
 echo '<NetworkConfiguration xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://schemas.microsoft.com/ServiceHosting/2011/07/NetworkConfiguration">' >> $VNETCONF
 echo '  <VirtualNetworkConfiguration>' >> $VNETCONF
 echo '    <Dns />' >> $VNETCONF
 echo '      <VirtualNetworkSites>' >> $VNETCONF
-echo "        <VirtualNetworkSite name=$input_vnet_name AffinityGroup=$input_affinity_group>" >> $VNETCONF
+echo $("        <VirtualNetworkSite name=" + '"' + "$input_vnet_name" + '"' + ' ' + "AffinityGroup=" + '"' + "$input_affinity_group" + '"' + ">") >> $VNETCONF
 echo '          <AddressSpace>' >> $VNETCONF
 echo "            <AddressPrefix>$input_address_prefix</AddressPrefix>" >> $VNETCONF
 echo '          </AddressSpace>' >> $VNETCONF
@@ -35,11 +28,11 @@ echo '          <Subnets>' >> $VNETCONF
 $subnet_num = $i + 1
 $input_subnet_name[$i] = Read-Host "Please enter Subnet name (number $subnet_num)"
 $input_subnet_prefix[$i] = Read-Host "Please enter Subnet prefix (number $subnet_num)"
-$ret_subnet_name = $input_subnet_name[$i]
-$ret_subnet_prefix = $input_subnet_name[$i]
+$use_subnet_name = $input_subnet_name[$i]
+$use_subnet_prefix = $input_subnet_prefix[$i]
 
-echo $("            <Subnet name=" + "$ret_subnet_name" + ">") >> $VNETCONF
-echo $("              <AddressPrefix>" + "$ret_subnet_prefix" + "</AddressPrefix>") >> $VNETCONF
+echo $("            <Subnet name=" + '"' + "$use_subnet_name" + '"' + ">") >> $VNETCONF
+echo $("              <AddressPrefix>" + "$use_subnet_prefix" + "</AddressPrefix>") >> $VNETCONF
 echo '            </Subnet>' >> $VNETCONF
 echo '          </Subnets>' >> $VNETCONF
 }
